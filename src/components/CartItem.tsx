acf732161c97d75b79d75284dcc9e38ca071aa58
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2022-10-06 20:06:37 +0100

    cartitem and storetsx

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -14,11 +14,11 @@ export function CartItem({ id, quantity }: CartItemProps) {
 
     return (
         <Stack direction='horizontal' gap={2}>
-            <img src={item.imgUrl}
+            <img src={item?.imgUrl}
             style={{width: '125px', height: '75px', objectFit: 'cover'}}/>
             <div className='me-auto'>
                 <div>
-                    {item.name} {' '}
+                    {item?.name} {' '}
                      {quantity > 1 && (
                         <span className='text-muted' style={{ fontSize: '.65rem'}}> 
                         x{quantity}</span>
@@ -32,4 +32,4 @@ export function CartItem({ id, quantity }: CartItemProps) {
             </div>
         </Stack>
     )
-}
+};
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -16,3 +16,4 @@ export function Store() {
     </>
     )
 };
+export {};
