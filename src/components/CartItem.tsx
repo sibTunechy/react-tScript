89196e559a4adddbd843c4a23259a061d6dc9377
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2022-10-06 21:46:23 +0100

    more chabges from github original

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,35 +1,85 @@
-import storeItems from '../data/items.json';
-import {Stack, Button } from 'react-bootstrap';
-import {useShoppingCart} from '../context/ShoppingCartContext';
-import { formatcurrency } from '../utilities/formatCurrency';
+import { Button, Stack } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import storeItems from "../data/items.json"
+import { formatcurrency } from "../utilities/formatCurrency"
 
 type CartItemProps = {
-    id: number
-    quantity: number
+  id: number
+  quantity: number
 }
 
 export function CartItem({ id, quantity }: CartItemProps) {
-    const {removeFromCart} = useShoppingCart()
-    const item = storeItems.find(i => i.id === id)
+  const { removeFromCart } = useShoppingCart()
+  const item = storeItems.find(i => i.id === id)
+  if (item == null) return null
 
-    return (
-        <Stack direction='horizontal' gap={2}>
-            <img src={item?.imgUrl}
-            style={{width: '125px', height: '75px', objectFit: 'cover'}}/>
-            <div className='me-auto'>
-                <div>
-                    {item?.name} {' '}
-                     {quantity > 1 && (
-                        <span className='text-muted' style={{ fontSize: '.65rem'}}> 
-                        x{quantity}</span>
-                     )}
-                </div>
-                <div className='text-muted' style={{fontSize: '.75rem'}}>
-                    {formatcurrency(item.price)}
-                </div>
-                <div>{formatcurrency(item.price * quantity)}</div>
-                <Button variant='outline-danger' size='sm' onClick={() => removeFromCart(item.id)}>&times;</Button>
-            </div>
-        </Stack>
-    )
-};
+  return (
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatcurrency(item.price)}
+        </div>
+      </div>
+      <div> {formatcurrency(item.price * quantity)}</div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
+    </Stack>
+  )
+}
+
+
+
+
+
+// import storeItems from '../data/items.json';
+// import {Stack, Button } from 'react-bootstrap';
+// import {useShoppingCart} from '../context/ShoppingCartContext';
+// import { formatcurrency } from '../utilities/formatCurrency';
+
+// type CartItemProps = {
+//     id: number
+//     quantity: number
+// }
+
+// export function CartItem({ id, quantity }: CartItemProps) {
+//     const {removeFromCart} = useShoppingCart()
+//     const item = storeItems.find(i => i.id === id)
+
+//     return (
+//         <Stack direction='horizontal' gap={2}>
+//             <img src={item?.imgUrl}
+//             style={{width: '125px', height: '75px', objectFit: 'cover'}}/>
+//             <div className='me-auto'>
+//                 <div>
+//                     {item?.name} {' '}
+//                      {quantity > 1 && (
+//                         <span className='text-muted' style={{ fontSize: '.65rem'}}> 
+//                         x{quantity}</span>
+//                      )}
+//                 </div>
+//                 <div className='text-muted' style={{fontSize: '.75rem'}}>
+//                     {formatcurrency(item.price)}
+//                 </div>
+//                 <div>{formatcurrency(item.price * quantity)}</div>
+//                 <Button variant='outline-danger' size='sm' onClick={() => removeFromCart(item.id)}>&times;</Button>
+//             </div>
+//         </Stack>
+//     )
+// };
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,19 +1,41 @@
-import storeItems from '../data/items.json';
-import {Row, Col} from 'react-bootstrap';
-import { StoreItem } from '../components/StoreItems';
+import { Col, Row } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItems"
+import storeItems from "../data/items.json"
 
 export function Store() {
-    return (
+  return (
     <>
-    <h1>Store</h1>
-    <Row md={2} xs={1} lg={3} className='g-3'>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map(item => (
-            <Col key={item.id}>
-                <StoreItem {...item}/>
-            </Col>
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
         ))}
-    </Row>
+      </Row>
     </>
-    )
+  )
 };
-export {};
+
+
+
+
+// import storeItems from '../data/items.json';
+// import {Row, Col} from 'react-bootstrap';
+// import { StoreItem } from '../components/StoreItems';
+
+// export function Store() {
+//     return (
+//     <>
+//     <h1>Store</h1>
+//     <Row md={2} xs={1} lg={3} className='g-3'>
+//         {storeItems.map(item => (
+//             <Col key={item.id}>
+//                 <StoreItem {...item}/>
+//             </Col>
+//         ))}
+//     </Row>
+//     </>
+//     )
+// };
+// export {};
