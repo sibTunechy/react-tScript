3b7777c711ed8282b36402da56a1f32aa5c1e99d
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2022-10-07 10:26:47 +0100

    ?

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -8,13 +8,14 @@ export function Store() {
       <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map(item => (
-          <Col key={item.id}>
+          <Col key={item?.id}>
             <StoreItem {...item} />
           </Col>
         ))}
       </Row>
     </>
   )
+
 };
 
 
